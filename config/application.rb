e6447583edb6482ee46e3901cea6374bb36d2b59
Serhii Budnik <s.budnik1990@gmail.com>
2017-06-26 23:32:22 +0300

    new branch

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module HeroesAndMagic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += %W(#{Rails.root}/app/modules)
+    config.autoload_paths += %W(#{Rails.root}/app/services)
+    config.autoload_paths += %W(#{Rails.root}/app/services/modules)
+    config.autoload_paths += %W(#{Rails.root}/app/services/classes)
+    config.autoload_paths += %W(#{Rails.root}/app/services/classes/interfaces)
+    config.autoload_paths += %W(#{Rails.root}/app/services/buffs/mage)
+    config.autoload_paths += %W(#{Rails.root}/app/services/buffs/priest)
+    config.autoload_paths += %W(#{Rails.root}/app/services/buffs/warrior)
+    config.autoload_paths += %W(#{Rails.root}/app/services/buffs/public)
+
     config.generators do |g|
       g.template_engine :haml
       g.test_framework          :rspec, fixtures: true, view: false
